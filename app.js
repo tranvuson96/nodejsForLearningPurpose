a8d7350c854e2be71b7f5ad05a06d43ecd9300d7
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-01-30 17:11:40 +0700

    lab1.5

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
 	const url = req.url;
+	const method = req.method;
 	if (url === "/") {
 		res.write("<html>");
 		res.write("<head><title>Enter Message</title></head>");
@@ -10,6 +12,12 @@ const server = http.createServer((req, res) => {
 			"<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>",
 		);
 		res.write("</html>");
+		return res.end();
+	}
+	if (url === "/message" && method === "POST") {
+		fs.writeFileSync("message.txt", "DUMMY");
+		res.statusCode = 302;
+		res.setHeader("Location", "/");
 		return res.end();
 	}
 	res.setHeader("Content-Type", "text/html");
