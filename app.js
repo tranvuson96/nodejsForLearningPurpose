34108de2120b4d0af31ef9a7a4e08e12af84f104
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-01 06:36:10 +0700

    lab2.2

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const express = require("express");
 
 const app = express();
 
+app.use((res, req, next) => {
+	console.log("In the middleware");
+	next(); //alow does res to continue to the next middleware in line
+});
+
+app.use((res, req, next) => {
+	console.log("In another middleware");
+	// ...
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
