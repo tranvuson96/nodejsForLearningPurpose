ae7df823a42a9af6f359fda79f0b90facbabf1fb
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-03 06:56:37 +0700

    lab2.13

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
+const rootdir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-	res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+	res.sendFile(path.join(rootdir, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
+const rootdir = require("../util/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+	res.sendFile(path.join(rootdir, "views", "shop.html"));
 });
 
 module.exports = router;
