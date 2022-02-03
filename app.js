70903aede881268f690a6a35dc56a5bc83f30349
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-04 04:44:21 +0700

    lab3.1

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 const path = require("path");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,15 @@
-const express = require("express");
 const path = require("path");
-const rootdir = require("../util/path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	res.sendFile(path.join(rootdir, "views", "shop.html"));
+	console.log(adminData.products);
+	res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
