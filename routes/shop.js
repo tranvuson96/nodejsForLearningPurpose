5779857e0ec2c7c1fa9fde5ae455d8e82963c5d7
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-06 05:47:42 +0700

    lab3.6

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
 	console.log(adminData.products);
 	const products = adminData.products;
-	res.render("shop", { prods: products, docTitle: "Shop" });
+	res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
 });
 
 module.exports = router;
