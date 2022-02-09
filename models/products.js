da045292f4e3f8e3d3f786cacd70e2453a0cd90e
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-09 21:16:29 +0700

    lab4.4

--- models/products.js
+++ models/products.js
@@ -1,4 +1,5 @@
-const products=[];
+const fs=require('fs');
+const path=require('path');
 
 module.exports=class Product {
     constructor(t){
@@ -6,10 +7,28 @@ module.exports=class Product {
     }
 
     save(){
-        products.push(this)
+        const p=path.join(path.dirname(require.main.filename),'data','products.json');
+        fs.readFile(p,(err,fileContent)=>{
+            console.log(fileContent);
+            let products=[];
+            if(!err){
+                products=JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err);
+            });
+        });
     }
 
     static fetchAll(){
+        const p=path.join(path.dirname(require.main.filename),'data','products.json');
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return [];
+            };
+            return JSON.parse(fileContent);
+        })
         return products;
     }
 }
