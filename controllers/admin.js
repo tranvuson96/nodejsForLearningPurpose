ca1765d9ee8318d473e805d8de00e33422ba814c
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-14 05:49:37 +0700

    lab4.14

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -21,11 +22,18 @@ exports.getEditProduct = (req, res, next) => {
   const editMode=req.query.edit;
   if (!editMode){
     return res.redirect('/');
-  };
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  }
+  const prodId=req.params.productId;
+  Product.findById(prodId,product=>{
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
   });
 };
 
