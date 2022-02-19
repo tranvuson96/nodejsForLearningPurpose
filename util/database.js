0cb40f8c73d0bf895d70cf1d75dc0ac29bf0a708
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-19 21:17:13 +0700

    lab5.08

--- util/database.js
+++ util/database.js
@@ -1,10 +1,9 @@
-const mysql= require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool=mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'danteandvergil1'
+const sequelize = new Sequelize('node-complete','root','danteandvergil1',
+{
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
