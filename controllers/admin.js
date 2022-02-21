82b718478c6c91d09f7fcd573ac1530de4f1a422
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-02-21 10:56:15 +0700

    lab5.15

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,8 +33,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
+  Product.findByPk(prodId)
+  .then(product=>{
+    if(!product){
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
@@ -43,7 +44,8 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
-  });
+  })
+  .catch(err=>console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -52,15 +54,19 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product=>{
+    product.title=updatedTitle;
+    product.price=updatedPrice;
+    product.description=updatedDesc;
+    product.imageUrl=updatedImageUrl;
+    return product.save();
+  })
+  .then(result=>{
+    console.log('UPDATED PRODUCT');
+    res.redirect('/admin/products');
+  })
+  .catch(err=>console.log(err))
 };
 
 exports.getProducts = (req, res, next) => {
