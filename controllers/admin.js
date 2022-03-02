fdc5fee360d3abe939ff51b8978e8ceca303fe52
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-02 21:26:13 +0700

    lab6.11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,6 @@
 const mongodb = require('mongodb');
 const Product = require('../models/product');
 
-const ObjectId = mongodb.ObjectId;
-
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
@@ -55,7 +53,7 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const product = new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl, new ObjectId(prodId))
+  const product = new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl,prodId)
   product.save()
   .then(result=>{
     console.log('UPDATED PRODUCT');
