b68a1c7f86ecc9b339904d265862e6fdda959ce4
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-03 19:49:54 +0700

    lab6.14

--- models/user.js
+++ models/user.js
@@ -1,16 +1,23 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database');
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
-});
+class User {
+    constructor(username,email) {
+        this.name = username;
+        this.email = email;
+    }
+
+    save() {
+        const db = getDb();
+        return db.collection('users').insertOne(this);
+    }
+
+    static findByPk(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({_id: new ObjectId(userId)});
+    }
+}
 
 module.exports = User;
