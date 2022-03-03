e483e8e139a95881672ee9f0af0323a338fba464
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-03 20:44:59 +0700

    lab6.17

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req,res,next)=>{
     User.findByPk('6220b89a071be8343ba22b0a')
     .then(user=>{
-        req.user=user;
+        req.user= new User(user.name,user.email,user.cart,user._id);
         next();
     })
     .catch(err=>console.log(err));
