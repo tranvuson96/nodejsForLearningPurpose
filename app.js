380076b11a01951b4f70c81d2991b7337cd2960f
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-04 19:59:18 +0700

    lab7.08

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req,res,next)=>{
-//     User.findByPk('6220b89a071be8343ba22b0a')
-//     .then(user=>{
-//         req.user= new User(user.name,user.email,user.cart,user._id);
-//         next();
-//     })
-//     .catch(err=>console.log(err));
-// });
+app.use((req,res,next)=>{
+    User.findById('62220cc34bb64fd0bff5a844')
+    .then(user=>{
+        req.user= user;
+        next();
+    })
+    .catch(err=>console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,6 +34,18 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://sontvfx11243:aoeXszMuk6HEIiNO@cluster0.anl8k.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result=>{
+        User.findOne().then(user=>{
+            if(!user){
+                const user = new User({
+                    name: 'Max',
+                    email: 'max@test.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
         app.listen(3000);
     })
     .catch(err=>{
