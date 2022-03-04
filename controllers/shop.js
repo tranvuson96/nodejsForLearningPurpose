b6eaec60ccaae22c8dc34998d9b365ed785f0d3f
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-04 21:39:18 +0700

    lab7.11

--- controllers/shop.js
+++ controllers/shop.js
@@ -57,7 +57,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
+  Product.findById(prodId)
     .then(product=>{
       return req.user.addToCart(product);
     })
