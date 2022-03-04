b7b00b78f4e0cd7e5a3d7dec599c0f5e8cabe6d9
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-04 22:20:55 +0700

    lab7.14

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -81,8 +82,21 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req,res,next)=>{
-  let fetchedCart;
-  req.user.addOrder()
+  req.user
+    .populate('cart.items.productId')
+    .then(user=>{
+      const products = user.cart.items.map(i=>{
+        return {quantity: i.quantity, product: i.productId};
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      order.save()
+    })
     .then(result=>{
       res.redirect('/orders');
     })
