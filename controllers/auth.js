c19a867f4786603aa0a173dff9e8d17f105bc5de
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-18 20:29:23 +0700

    lab8.03

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,5 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn=req.get('Cookie').split(';')[0].trim().split('=')[2];
+    const isLoggedIn=req.get('Cookie').split(';')[0].trim().split('=')[2] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
