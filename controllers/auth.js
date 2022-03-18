093170109956406cad833798518f5fe8433be8dc
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-18 20:45:44 +0700

    lab08.04

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn=req.get('Cookie').split(';')[0].trim().split('=')[2] === 'true';
+    // const isLoggedIn=req.get('Cookie').split(';')[0].trim().split('=')[2] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: isLoggedIn
+        isAuthenticated: false
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn==true')
+    // Expries=  ; Max-Age=10 ; Secure; Domain= ;HttpOnly
+    res.setHeader('Set-Cookie', 'loggedIn==true; HttpOnly')
     res.redirect('/');
 };
