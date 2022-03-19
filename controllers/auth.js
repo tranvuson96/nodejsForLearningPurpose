772662b9acff8d7d50d3746aecc178cf514f2350
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-19 22:12:15 +0700

    lab8.27

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,16 +16,22 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if(message.length>0){
+        message = message[0];
+    } else {
+        message = null;
+    }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message
   });
 };
 
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-
   User.findOne({email:email})
     .then(user => {
         if(!user) {
@@ -60,6 +66,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
+          req.flash('error', 'Email exists already, please pick a diffone');
         return res.redirect('/signup');
       }
       return bcrypt
