1534fb1e03740de827f0355a1ffa195ea6a88c76
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-20 15:44:54 +0700

    lab9.04

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -12,14 +12,23 @@ router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
 
 router.post('/signup', 
-check('email')
-.isEmail()
-.withMessage('Please enter a valid email')
-.custom((value,{req})=>{
-    if(value === 'test@test.com') {
-        throw new Error('This email address is forbidden!')
-    }
-})
+[
+    check('email')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value,{req})=>{
+        if(value === 'test@test.com') {
+            throw new Error('This email address is forbidden!')
+        }
+        return true;
+    }),
+    body(
+        'password',
+        'Please enter a password with only numbers and text and at least 5 charaters'
+    )
+        .isLength({min:5})
+        .isAlphanumeric()
+]
 , authController.postSignup);
 
 router.post('/logout', authController.postLogout);
