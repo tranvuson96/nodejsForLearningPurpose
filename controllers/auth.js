87041af7b2ab21552fe3eec2e21e9ffc289a9466
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-20 20:30:56 +0700

    lab9.07

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,7 +40,12 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
   });
 };
 
@@ -51,7 +56,7 @@ exports.postLogin = (req, res, next) => {
 
   if(!errors.isEmpty()) {
     console.log( errors.array()[0].msg);
-    return res.render('auth/login', {
+    return res.status(422).render('auth/login', {
       path: '/login',
       pageTitle: 'Login',
       errorMessage: errors.array()[0].msg
@@ -92,11 +97,11 @@ exports.postSignup = (req, res, next) => {
   const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {email: email, password:password, confirmPassword: confirmPassword }
     });
   }
       bcrypt
