46a213c0469252d0ebb7d75290e82afe1761d2d3
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-22 22:04:15 +0700

    lab9.18

--- app.js
+++ app.js
@@ -74,12 +74,12 @@ app.get('/500',errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect('/500');
-  // res.status(500).render('500', {
-  //   pageTitle: 'Error!',
-  //   path: '/500',
-  //   isAuthenticated: req.session.isLoggedIn
-  // });
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 });
 
 mongoose
