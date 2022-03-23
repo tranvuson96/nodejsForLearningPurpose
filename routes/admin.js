f66d206b2799f62d172437ce6c1867da6ba5e826
Tran Vu Son <sontvfx11243@funix.edu.vn>
2022-03-23 14:54:45 +0700

    lab10.02

--- routes/admin.js
+++ routes/admin.js
@@ -20,8 +20,6 @@ router.post('/add-product',[
         .isString()
         .isLength({min: 3 })
         .trim(),
-    body('imageUrl')
-        .isURL(),
     body('price')
         .isFloat(),
     body('description')
